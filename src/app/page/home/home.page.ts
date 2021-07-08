9fdb9a79641819be12956e8c8d3d1ada61b4b35b
Victoria Derudder <viki.derudder@gmail.com>
2021-07-08 15:00:00 -0300

    wip: showing order's actual total in toast

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -10,6 +10,9 @@ import { ModalFoodComponent } from '../../modal/modal-food/modal-food.component'
 export class HomePage {
   readonly foodFolder = "../../assets/img/food/";
 
+  public total: number = 0;
+
+  //Datos falsos
   public promos = {
     titulo: 'Promos',
     items: [
@@ -139,6 +142,7 @@ export class HomePage {
       ]
   }
 
+  /**Obtiene la lista de categorias */
   public get Categorias() {
     return [this.promos, this.pizzas, this.minutas, this.empanadas, this.bebidas]
   }
@@ -151,10 +155,12 @@ export class HomePage {
 
   }
 
+  /**Muestra el modal de informacion de comida */
   public clickFood(food) {
     this.presentModal(food);
   }
 
+  /**Crea el modal */
   async presentModal(food) {
     const modal = await this.modalController.create({
       component: ModalFoodComponent,
@@ -166,14 +172,16 @@ export class HomePage {
     return await modal.present();
   }
 
+  /**Muestra el toast con el total */
   public mostrarTotal() {
     this.presentToastWithOptions();
   }
 
+  /**Crea el toast */
   async presentToastWithOptions() {
     const toast = await this.toastController.create({
       header: 'Total',
-      message: '$1200', //TODO: poner la suma total del pedido
+      message: '$' + this.total.toString(),
       position: 'bottom',
       buttons: [
         {
@@ -189,26 +197,57 @@ export class HomePage {
     await toast.present();
   }
 
+  /**Redirige a la categoria seleccionada en el menu */
   public clickCategoria(idCategoria: string) {
+    //Obtiene el elemento, se redirige a esa zona de la pagina y cierra el menu
     const element = document.getElementById(idCategoria);
     element.scrollIntoView({ behavior: "smooth", block: 'start' });
     this.menuController.close();
   }
 
-  public agregarCantidad(food) {
+  /**Agrega el producto al pedido, sumandolo al total */
+  public agregarProducto(food) {
+    //Se fija si hay alguna cantidad
     if (food.cantidad) {
       food.cantidad += 1;
     }
+    //Cantidad en 0
     else {
       food.cantidad = 1;
     }
+    
+    //Se fija si el total es 0, suma el producto y muestra el total
+    if (this.total == 0) {
+      this.total = this.total + food.price;
+      this.mostrarTotal(); 
+    }
+    //El total ya es mayor que 0, por lo que suma el producto, borra el toast anterior y crea uno nuevo con el total actualizado
+    else {
+      this.total = this.total + food.price;
+      this.toastController.dismiss();
+      this.mostrarTotal();
+    }
 
-    this.mostrarTotal();
   }
 
-  public quitarCantidad(food) {
+  /**Quita el producto al pedido, restandolo del total */
+  public quitarProducto(food) {
+    //Se fija que la cantidad este en 0 para no pasar a nros negativos
     if (food.cantidad > 0) {
       food.cantidad -= 1;
+    }
+    
+    //Resta el producto del total
+    this.total = this.total - food.price;
+
+    //Si el total esta en 0, no muestra ningun toast con total
+    if (this.total == 0) {
+      this.toastController.dismiss();
+    }
+    //Si el total es mayor que 0, borra el toast anterior y crea uno nuevo con el total actualizado
+    else {
+      this.toastController.dismiss();
+      this.mostrarTotal();
     }
 
   }
