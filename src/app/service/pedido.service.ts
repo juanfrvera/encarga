a98d4c078ead342f11aa640bc827a59f1a347ada
Victoria Derudder <viki.derudder@gmail.com>
2021-07-08 20:40:56 -0300

    wip: added get and remove methods, modified add method

--- src/app/service/pedido.service.ts
+++ src/app/service/pedido.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Item } from '../data/item';
+import { LineaPedido } from '../data/lineaPedido';
 
 @Injectable({
   providedIn: 'root'
@@ -7,22 +8,48 @@ import { Item } from '../data/item';
 export class PedidoService {
 
   private static readonly storageKey = "pedidos";
+  public total: number = 0;
 
   constructor() { }
+  
+  public get() {
+    const pedidosJson = localStorage.getItem(PedidoService.storageKey);
+    const pedidos = pedidosJson ? JSON.parse(pedidosJson) as LineaPedido[] : []
+    return pedidos;
+  }
 
   public add(item: Item) {
     // Get from localstorage
     const pedidosJson = localStorage.getItem(PedidoService.storageKey);
-    const pedidos = pedidosJson ? JSON.parse(pedidosJson) as Item[] : [];
+    const pedidos = pedidosJson ? JSON.parse(pedidosJson) as LineaPedido[] : [];
 
-    // Add item
-    pedidos.push(item);
+    const lineaPedido = pedidos.find(p => p.idItem == item.id);
+
+    if (!lineaPedido)
+      // Add item
+      pedidos.push({ idItem: item.id, cantidad: 1 });
+    else
+      lineaPedido.cantidad++;
 
     // Save to localstorage
     localStorage.setItem(PedidoService.storageKey, JSON.stringify(pedidos));
   }
 
-  public remove(item : Item){
-    
+  public remove(item: Item) {
+    // Get from localstorage
+    const pedidosJson = localStorage.getItem(PedidoService.storageKey);
+    const pedidos = pedidosJson ? JSON.parse(pedidosJson) as LineaPedido[] : [];
+
+    const lineaPedido = pedidos.find(p => p.idItem == item.id);
+    const indexLineaPedido = pedidos.indexOf(lineaPedido);
+
+    if (lineaPedido.cantidad == 1)
+      // Remove item
+      pedidos.splice(indexLineaPedido, 1);
+    else
+      lineaPedido.cantidad--;
+
+    // Save to localstorage
+    localStorage.setItem(PedidoService.storageKey, JSON.stringify(pedidos));
   }
 }
