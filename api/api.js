4abebc458b70c1b3e4a351b5ca9b5ad83a9c003a
Juan Vera <juanfrvera@gmail.com>
2021-07-09 15:29:32 -0300

    heroku: listen to heroku port if there is any

--- api/api.js
+++ api/api.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+
+// heroku port, 3000 if we are local
+const port = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
