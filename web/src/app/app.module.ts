7a24ee040e39b6904ab3e331e91b7fa744d3c681
Juan Vera <juanfrvera@gmail.com>
2021-07-11 15:07:52 -0300

    feat-web: api service

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,11 +10,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ web/src/app/service/api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+
+export class ApiService<T> {
+
+  private readonly url: string = 'https://encargarpedido.herokuapp.com/';
+  private readonly route: string;
+
+  constructor(private http: HttpClient, route: string) {
+    this.route = route;
+  }
+
+  public create(data: T) {
+    return this.http.post(this.url + this.route, data);
+  }
+  public getAll() {
+    return this.http.get<T[]>(this.url + this.route);
+  }
+  public getById(id: string) {
+    return this.http.get<T>(this.url + this.route + id);
+  }
+  public updateById(id: string, data: T) {
+    return this.http.put<T>(this.url + this.route + id, data);
+  }
+  public deleteById(id: string) {
+    return this.http.delete<T>(this.url + this.route + id);
+  }
+}
