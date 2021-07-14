fa67086bc9f7cf1692c17f444d5ad20651a495c2
Juan Vera <juanfrvera@gmail.com>
2021-07-13 22:36:58 -0300

    fix-api: return created entity on item controller

--- api/src/controller/item.controller.js
+++ api/src/controller/item.controller.js
@@ -3,16 +3,11 @@ const db = require("../config/database");
 exports.create = async (req, res) => {
     const { titulo, precio, descripcion } = req.body;
     const { rows } = await db.query(
-        "INSERT INTO item (titulo, precio, descripcion) VALUES ($1, $2, $3)",
+        "INSERT INTO item (titulo, precio, descripcion) VALUES ($1, $2, $3) RETURNING *",
         [titulo, precio, descripcion]
     );
 
-    res.status(201).send({
-        message: "Item added successfully!",
-        body: {
-            item: { titulo, precio, descripcion }
-        },
-    });
+    res.status(201).send(rows[0]);
 };
 
 exports.getAll = async (req, res) => {
