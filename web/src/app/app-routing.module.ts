aec4c8157cd85129af214618c1bda65cc6f59339
Victoria Derudder <viki.derudder@gmail.com>
2021-07-16 19:07:53 -0300

    ng: generated components for home, detalle, admin, landing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './page/admin/admin.component';
+import { DetalleComponent } from './page/detalle/detalle.component';
+import { HomeComponent } from './page/home/home.component';
+import { LandingComponent } from './page/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'detalle',
+    component: DetalleComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { DetalleComponent } from './page/detalle/detalle.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { LandingComponent } from './page/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetalleComponent,
+    AdminComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
