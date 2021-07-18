d9bab07db7fba2af0c6035313ee585a3cb150a2c
Juan Vera <juanfrvera@gmail.com>
2021-07-17 21:44:05 -0300

    feat: app routing skeleton

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './page/admin/admin.component';
-import { DetalleComponent } from './page/detalle/detalle.component';
-import { HomeComponent } from './page/home/home.component';
+
 import { LandingComponent } from './page/landing/landing.component';
+import { PedidoComponent } from './page/pedido/pedido.component';
+import { HomeComponent } from './page/pedido/home/home.component';
+import { DetalleComponent } from './page/pedido/detalle/detalle.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { ItemComponent } from './page/admin/item/item.component';
+import { CategoriaComponent } from './page/admin/categoria/categoria.component';
 
 const routes: Routes = [
   {
@@ -11,16 +15,20 @@ const routes: Routes = [
     component: LandingComponent
   },
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'detalle',
-    component: DetalleComponent
+    path: 'pedido',
+    component: PedidoComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'detalle', component: DetalleComponent }
+    ]
   },
   {
     path: 'admin',
-    component: AdminComponent
+    component: AdminComponent,
+    children: [
+      { path: '', component: ItemComponent },
+      { path: 'categoria', component: CategoriaComponent }
+    ]
   }
 
 
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './page/home/home.component';
-import { DetalleComponent } from './page/detalle/detalle.component';
-import { AdminComponent } from './page/admin/admin.component';
-import { LandingComponent } from './page/landing/landing.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PedidoComponent } from './page/pedido/pedido.component';
+import { AdminComponent } from './page/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DetalleComponent,
-    AdminComponent,
-    LandingComponent
+    PedidoComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
