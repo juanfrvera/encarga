89f1e69434fc5f6895cb97b95268e72c486da9e1
Juan Vera <juanfrvera@gmail.com>
2021-07-18 14:04:08 -0300

    bootstrap: javascript y popper traidos desde CDN para ahorrar ancho de banda
    Los estilos se dejaron con npm para poder hacerle override a las variables

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,9 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Código javascript de bootstrap
-import 'bootstrap';
-
 if (environment.production) {
   enableProdMode();
 }
