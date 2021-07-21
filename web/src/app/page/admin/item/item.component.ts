a63a5806c6bd28423c33114d04515e26a971590a
Juan Vera <juanfrvera@gmail.com>
2021-07-21 20:04:02 -0300

    feat: crear y editar item en admin

--- web/src/app/page/admin/item/item.component.ts
+++ web/src/app/page/admin/item/item.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Modal } from 'bootstrap';
+import { FormularioComponent } from '../../../component/formulario/formulario.component';
 import { Item } from '../../../data/item';
 import { ItemService } from '../../../service/item.service';
+import { Util } from '../../../util';
 
 @Component({
   selector: 'app-item',
@@ -9,8 +11,16 @@ import { ItemService } from '../../../service/item.service';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements AfterViewInit {
+  @ViewChild(FormularioComponent) formulario: FormularioComponent;
   @ViewChild('modal', { static: true }) modalElement: ElementRef;
   private modal: Modal;
+
+  /** Item en modal */
+  private item: Item = {} as Item;
+
+  public get Item() {
+    return this.item;
+  }
 
   public get Items() {
     return this.itemService.Items;
@@ -26,16 +36,45 @@ export class ItemComponent implements AfterViewInit {
 
   /** Muestra el modal en modo creacion */
   public agregar() {
+    this.limpiarItem();
     this.abrir();
   }
 
   /** Muestra el modal en modo edicion */
   public editar(item: Item) {
-    this.abrir(item);
+    this.item = Util.copiaProfunda(item);
+    this.abrir();
   }
 
-  private abrir(item?: Item) {
+  public guardar() {
+    if (this.formulario.esValido()) {
+      if (this.Item.id) {
+        // Editando
+        this.itemService.edit(this.Item);
+      }
+      else {
+        // Creando
+        this.itemService.create(this.Item);
+      }
+
+      this.cerrar();
+    }
+    else {
+      this.formulario.mostrarFeedback();
+    }
+  }
+
+  private abrir() {
+    this.formulario.ocultarFeedback();
     this.modal.show();
+  }
+
+  private cerrar() {
+    this.modal.hide();
+  }
+
+  private limpiarItem() {
+    this.item = {} as Item;
   }
 
 }
