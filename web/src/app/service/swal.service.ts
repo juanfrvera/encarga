35294e93861ccb720b3dcfdc0a05b894e965e9e2
Juan Vera <juanfrvera@gmail.com>
2021-07-22 19:39:44 -0300

    feat: agregado y configurado swal2
    Sweet Alert 2. Usando CDN, bootrap theming y Swal2 como dependencia dev en npm. Además se agregó archivo "sweet-alert.scss" y servicio swal.service para configurarlo

---
+++ web/src/app/service/swal.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import Swal, { SweetAlertIcon, SweetAlertOptions } from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SwalService {
+  private static readonly defaultOptions: SweetAlertOptions = {
+    customClass: {
+      confirmButton: 'confirm-button'
+    }
+  };
+
+  constructor() { }
+
+  /**
+  * Function to display a SweetAlert2 popup, with an object of options, all being optional.
+  * See the `SweetAlertOptions` interface for the list of accepted fields and values.
+  *
+  * Example:
+  * ```
+  * Swal.fire({
+  *   title: 'Auto close alert!',
+  *   text: 'I will close in 2 seconds.',
+  *   timer: 2000
+  * })
+  * ```
+  */
+  fire<T = any>(options: SweetAlertOptions<T>) {
+    // If objects have a property with the same name,
+    // then the right-most object property overwrites the previous one.
+    const mergedOptions: SweetAlertOptions<T> = {
+      ...SwalService.defaultOptions,
+      ...options
+    }
+
+    return Swal.fire<T>(mergedOptions);
+  }
+
+  /**
+     * Function to display a simple SweetAlert2 popup.
+     *
+     * Example:
+     * ```
+     * Swal.fire('The Internet?', 'That thing is still around?', 'question');
+     * ```
+     */
+  fireDefault<T = any>(title?: string, html?: string, icon?: SweetAlertIcon) {
+    return Swal.fire<T>(title, html, icon);
+  }
+}
