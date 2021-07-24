bde3c481c47cf1124a628c4a1394906637b6b586
Victoria Derudder <viki.derudder@gmail.com>
2021-07-24 15:09:05 -0300

    wip: added empty state for cart and deleted alert

--- web/src/app/page/pedido/detalle/detalle.component.ts
+++ web/src/app/page/pedido/detalle/detalle.component.ts
@@ -72,21 +72,6 @@ export class DetalleComponent implements OnInit {
 
     if (item.cantidad <= 0) {
       Util.eliminarItem(this.itemsConCantidad, item);
-
-      if (!this.itemsConCantidad.length) {
-        this.swalService.fire(
-          {
-            title: 'Oh, oh...',
-            text: 'Parece que tu pedido está vacío. Volvé para agregar ítems.',
-            confirmButtonText: 'Volver',
-            icon: 'info',
-            iconColor: '#fc453c'
-          }
-        ).then(res => {
-          if (res.isConfirmed)
-            this.router.navigateByUrl('/pedido');
-        });
-      }
     }
   }
 
