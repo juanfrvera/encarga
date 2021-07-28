0a367ac467d7f6b72582c94a056ec4369094158f
Juan Vera <juanfrvera@gmail.com>
2021-07-28 14:01:40 -0300

    wip: ids de categorias de items

--- web/src/app/page/admin/item/item.component.ts
+++ web/src/app/page/admin/item/item.component.ts
@@ -8,15 +8,6 @@ import { ItemService } from '../../../service/item.service';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent {
-  selectedCar: number;
-
-  cars = [
-    { id: 1, name: 'Volvo' },
-    { id: 2, name: 'Saab' },
-    { id: 3, name: 'Opel' },
-    { id: 4, name: 'Audi' },
-  ];
-
   public get Service() {
     return this.itemService;
   }
