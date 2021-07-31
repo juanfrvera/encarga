d3db69d9e37718f702a48415d2a0c75ade30040d
Juan Vera <juanfrvera@gmail.com>
2021-07-31 20:53:09 -0300

    web: usando anti-fouc para evitar carga inicial fea sin estilos

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
+  /** Flash Of Unstyled Content: usado para eliminar efecto feo de cargar la pagina sin estilos */
+  private fouc: boolean;
   title = 'web';
+
+  public get Fouc() {
+    return this.fouc;
+  }
+
+  ngAfterContentInit(): void {
+    this.fouc = true;
+  }
 }
