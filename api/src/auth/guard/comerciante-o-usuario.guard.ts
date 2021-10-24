456513b2bb12df36f720bde27714f980da4cb514
Juan Vera <juanfrvera@gmail.com>
2021-10-24 11:44:17 -0300

    fix: auth strategy

--- api/src/auth/guard/comerciante-o-usuario.guard.ts
+++ api/src/auth/guard/comerciante-o-usuario.guard.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class ComercioOVisitaGuard extends AuthGuard('jwt') {
+export class ComercioOVisitaGuard extends AuthGuard('comerciante') {
     canActivate(context: ExecutionContext) {
         const req = context.switchToHttp().getRequest();
 
