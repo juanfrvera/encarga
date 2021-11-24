fcbe801d8ecf7aa8673d12d8f132eec45c3a185d
Juan Vera <juanfrvera@gmail.com>
2021-11-24 17:36:08 -0300

    clean: unused dependencies in categoria controller

--- api/src/categoria/categoria.controller.ts
+++ api/src/categoria/categoria.controller.ts
@@ -1,13 +1,7 @@
 import { Controller } from '@nestjs/common';
-import { ComerciosService } from 'src/comercio/comercio.service';
-import { UsuarioComercioService } from 'src/usuario-comercio/usuario-comercio.service';
-import { CategoriaService } from './categoria.service';
 
 @Controller('categoria')
 export class CategoriaController {
-  constructor(
-    private readonly service: CategoriaService,
-    private readonly comercioService: ComerciosService,
-    private readonly usuarioComercioService: UsuarioComercioService) {
+  constructor() {
   }
 }
