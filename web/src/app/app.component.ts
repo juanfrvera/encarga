4c204c67bffae0e46bdde7c88dbcfb8707dccc78
Juan Vera <juanfrvera@gmail.com>
2022-01-09 18:28:58 -0300

    fix-front: stop using fouc

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,20 +1,10 @@
-import { AfterContentInit, AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterContentInit {
-  /** Flash Of Unstyled Content: usado para eliminar efecto feo de cargar la pagina sin estilos */
-  private fouc: boolean;
+export class AppComponent {
   title = 'web';
-
-  public get Fouc() {
-    return this.fouc;
-  }
-
-  ngAfterContentInit(): void {
-    this.fouc = true;
-  }
 }
