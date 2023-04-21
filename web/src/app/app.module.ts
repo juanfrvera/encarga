0d600632b5cb0cc25ecf95932a1aa86618b54bd9
Juan Vera <juanfrvera@gmail.com>
2023-04-21 09:11:09 -0300

    feat: before start steps

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { StartComponent } from './landing/page/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, StartComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
   ],
   bootstrap: [AppComponent]
 })
--- web/src/app/landing/page/start/start.component.ts
+++ web/src/app/landing/page/start/start.component.ts
@@ -1,8 +1,20 @@
 import { Component } from "@angular/core";
 
 @Component({
-    templateUrl: "./start.component.html"
+    templateUrl: "./start.component.html",
+    styleUrls: ["./start.component.scss"]
 })
 export class StartComponent {
+    public view = { disableSubmit: true, phone: "", name: "" };
 
+    public onPhoneKeyUp() {
+        this.checkSubmitDisable();
+    }
+    public onNameKeyUp() {
+        this.checkSubmitDisable();
+    }
+
+    private checkSubmitDisable() {
+        this.view.disableSubmit = this.view.phone == null || this.view.phone.length <= 0 || this.view.name == null || this.view.name.length <= 0
+    }
 }
