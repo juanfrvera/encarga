f3048075291ca9b192fde80b2f29d3e53136045c
Victoria Derudder <32909661+vderudder@users.noreply.github.com>
2023-05-30 16:34:25 -0300

    wip: cambios visuales al dashboard de admin

--- web/src/app/seller/page/admin.component.ts
+++ web/src/app/seller/page/admin.component.ts
@@ -1,28 +1,12 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
-import { Offcanvas } from 'bootstrap';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements AfterViewInit, OnDestroy {
-  // Contenedor donde se pondrá el offcanvas
-  @ViewChild('container', { static: true }) container: ElementRef;
-  @ViewChild('offcanvas', { static: true }) offcanvasElement: ElementRef;
-  private offcanvas: Offcanvas;
+export class AdminComponent {
 
   constructor() { }
-  ngAfterViewInit(): void {
-    this.offcanvas = new Offcanvas(this.offcanvasElement.nativeElement);
-  }
-  ngOnDestroy(): void {
-    // Ocultarlo para que no haya problemas con el scroll
-    this.offcanvas.hide();
-  }
-
-  public abrirMenu() {
-    this.offcanvas.show(this.container.nativeElement);
-  }
 
 }
